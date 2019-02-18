e3c7c6a465b37e183ec490dcf8727320aaf65ddd
Matt Hayes <matt@mysterycommand.com>
2019-02-18 11:09:22 -0500

    feat: arithmetic

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -14,8 +14,27 @@ fn main() {
     let x = x * 3 + 1;
     println!("The value of x is: {}", x);
 
-
     let x = 2.0;
     let y: f32 = 3.0;
     println!("The value of x is: {}, and y is: {}", x, y);
+
+    // addition
+    let sum = 5 + 10;
+    println!("The sum is: {}", sum);
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+    println!("The difference is: {}", difference);
+
+    // multiplication
+    let product = 4 * 30;
+    println!("The product is: {}", product);
+
+    // division
+    let quotient = 56.7 / 32.2;
+    println!("The quotient is: {}", quotient);
+
+    // remainder
+    let remainder = 43 % 5;
+    println!("The remainder is: {}", remainder);
 }
