6bfda3ebd8af1203727b78d68d6e1db31aa937d6
Matt Hayes <matt@mysterycommand.com>
2019-02-18 17:31:16 -0500

    feat: methods with arguments

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -8,17 +8,23 @@ impl Rect {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rect) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
-    let rect = Rect {
-        width: 30,
-        height: 50,
-    };
+    let rect1 = Rect { width: 30, height: 50 };
+    let rect2 = Rect { width: 10, height: 40 };
+    let rect3 = Rect { width: 60, height: 45 };
 
-    println!("rect is {:#?}", rect);
+    println!("rect1 is {:#?}", rect1);
     println!(
-        "The area of the rectangle is {} square pixels.",
-        rect.area(),
+        "The area of rect1 is {} square pixels.",
+        rect1.area(),
     );
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
