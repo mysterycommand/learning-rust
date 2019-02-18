2d86cf6226535b2c0f47b2ac7b4dd53bd6452c3f
Matt Hayes <matt@mysterycommand.com>
2019-02-18 13:46:08 -0500

    feat: for loops and iterators

--- branches-n-loops/src/main.rs
+++ branches-n-loops/src/main.rs
@@ -44,4 +44,9 @@ fn main() {
         number = number - 1;
     }
     println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
 }
