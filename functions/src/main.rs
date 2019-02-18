6f11e07faef51b065c2ff56c9f2b3eb611855df0
Matt Hayes <matt@mysterycommand.com>
2019-02-18 13:18:53 -0500

    refactor: functions with return values and types

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
     println!("Hello, world!");
-    another_function(5, 6);
+    another_function(five(), 6);
 }
 
 fn another_function(x: i32, y: i32) {
     println!("Another function!");
     println!("The value of x is: {}", x);
     println!("The value of y is: {}", y);
+}
+
+fn five() -> i32 {
+    5
 }
