9044ebc9ecd034985a5d853636135da15a4362be
Matt Hayes <matt@mysterycommand.com>
2019-02-18 17:01:00 -0500

    feat: calculating the product of width and height

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let width = 30;
+    let height = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width, height)
+    );
+}
+
+fn area(w: i32, h: i32) -> i32 {
+    w * h
 }
