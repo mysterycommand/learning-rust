094aa2d320c8fea926412cbe53f80266944e84b3
Matt Hayes <matt@mysterycommand.com>
2019-02-18 13:13:19 -0500

    refactor: function parameters

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,10 @@
 fn main() {
     println!("Hello, world!");
-    another_function();
+    another_function(5, 6);
 }
 
-fn another_function() {
+fn another_function(x: i32, y: i32) {
     println!("Another function!");
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
 }
